39c3f45745ce56130317851cd7dedab958407bf7
anitastipic <mailforanitastipic@gmail.com>
2023-11-22 12:00:10 +0100

    made navbar sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
 
 
     return(
-       <div className="navContainer flex bg-gray-950 text-amber-50 pt-2 h-[5vh]">
+       <div className="sticky top-0 navContainer flex bg-gray-950 text-amber-50 pt-2 h-[5vh]">
            <p className={"navButton ml-5 mr-10"}>Hard Facts</p>
            <p className={"navButton mr-10"}>Experience</p>
            <p className={"navButton mr-10"}>Skillset</p>
