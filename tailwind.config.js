fb29d1e4d4535124e23a458d908fe6f01515fff6
anitastipic <mailforanitastipic@gmail.com>
2023-11-22 16:04:36 +0100

    removed shadow configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ export default {
     extend: {
       colors: {
       },
-      dropShadow: {
-        glow: [
-          "0 0px 20px rgba(255,255, 255, 0.35)",
-          "0 0px 65px rgba(255, 255,255, 0.2)"
-        ]
-      }
     },
   },
   plugins: [],
