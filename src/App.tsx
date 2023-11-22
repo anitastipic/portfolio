f8d87e66003400327913f0296e8cff89050bc7bd
anitastipic <mailforanitastipic@gmail.com>
2023-11-22 12:01:02 +0100

    added version 2 of home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar.tsx";
 import HomeLogo from "./components/HomeLogo.tsx";
+import HomeLogo2 from "./components/HomeLogo2.tsx";
 
 function App() {
 
     return (
-        <div>
+        <div className="bg-black">
             <Navbar/>
-            <div className="bg-gray-950 flex flex-col justify-normal
+            <div className="bg-black flex flex-col justify-normal
              items-center text-center">
+                <HomeLogo2/>
                 <HomeLogo/>
             </div>
         </div>
