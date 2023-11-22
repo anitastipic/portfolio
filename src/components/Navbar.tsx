e193ec40cffea589d7ae4d25e9a439f19b2c3f73
anitastipic <mailforanitastipic@gmail.com>
2023-11-22 11:13:32 +0100

    added simple navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+
+
+
+    return(
+       <div className="navContainer flex bg-gray-950 text-amber-50 pt-2 h-[5vh]">
+           <p className={"navButton ml-5 mr-10"}>Hard Facts</p>
+           <p className={"navButton mr-10"}>Experience</p>
+           <p className={"navButton mr-10"}>Skillset</p>
+           <p className={"navButton mr-10"}>Projects</p>
+       </div>
+    )
+}
+
+export default Navbar
