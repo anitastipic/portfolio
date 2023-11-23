d4d5651b640acd7bc3bdd7ef4789e1b55cc8a59f
anitastipic <mailforanitastipic@gmail.com>
2023-11-23 12:50:19 +0100

    added shuffle functionality to memory

--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 
+
 export default function Memory() {
     const [rows, setRows] = useState<React.ReactElement[][]>([]);
     const totalCards = 10;
@@ -8,25 +9,37 @@ export default function Memory() {
     useEffect(() => {
         const fullRows: React.ReactElement[][] = [];
         let currentRow: React.ReactElement[] = [];
+        const allCards: React.ReactElement[] = [];
 
         for(let i = 1; i <= totalCards; i++) {
-            console.log(i);
             for(let j = 0; j <= 1; j++) {
-                if(currentRow.length >= elementsPerRow) {
-                    fullRows.push([...currentRow]);
-                    currentRow = [];
-                }
-                currentRow.push(<div key={`card${i}-${j}`} className="m-2 bg-white h-[15vh] w-[15vh] flex justify-center items-center"><img className="h-[10vh] w-auto" src={`/memory/card${i}.svg`} alt={`card image ${i}`}/></div>)
-                if(currentRow.length >= elementsPerRow) {
-                    fullRows.push([...currentRow]);
-                    currentRow = [];
-                }
+                allCards.push(<div key={`card${i}-${j}`} className="m-2 bg-white h-[15vh] w-[15vh] flex justify-center items-center"><img className="h-[10vh] w-auto" src={`/memory/card${i}.svg`} alt={`card image ${i}`}/></div>)
             }
         }
+
+        shuffleArray(allCards);
+
+        for(let i = 0; i <= allCards.length; i++) {
+            currentRow.push(allCards[i]);
+            if(currentRow.length >= elementsPerRow) {
+                fullRows.push([...currentRow]);
+                currentRow = [];
+            }
+        }
+
+
         setRows(fullRows)
     }, [totalCards, elementsPerRow]);
 
 
+    function shuffleArray(array: React.ReactElement[]) {
+        for(let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i + 1);
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
     return (
         <div className="bg-black h-screen p-10 flex items-center justify-center">
