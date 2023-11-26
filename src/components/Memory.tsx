bd80512c1f1ea53f9e22aef4e5b04adb9c4cf860
anitastipic <mailforanitastipic@gmail.com>
2023-11-26 22:25:56 +0100

    completed memory logic

--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -5,15 +5,33 @@ type Card = {
     src: string;
     chosen: boolean;
     className: string;
+    foundMatch: boolean;
 }
 
 
 export default function MemoryLogic() {
     const [cards, setCards] = useState<Card[]>([]);
+    const [openCards, setOpenCards] = useState<number[]>([]);
 
     useEffect(() => {
         setCards(generateCards(10));
     }, []);
+
+
+    useEffect(() => {
+        if (openCards.length === 2) {
+            const [firstIndex, secondIndex] = openCards;
+            if (cards[firstIndex].src === cards[secondIndex].src) {
+                setCards(prevCards =>
+                    prevCards.map((card, index) =>
+                        index === firstIndex || index === secondIndex
+                            ? {...card, foundMatch: true} : card
+                    )
+                );
+                setOpenCards([]);
+            }
+        }
+    }, [openCards, cards]);
 
     function generateCards(totalCardsPerType: number) {
         const generatedCards: Card[] = [];
@@ -22,13 +40,15 @@ export default function MemoryLogic() {
                 index: `card${i}-1`,
                 src: `/memory/card${i}.svg`,
                 chosen: false,
-                className: "cardimg object contain"
+                className: "cardimg object contain",
+                foundMatch: false
             })
             generatedCards.push({
                 index: `card${i}-2}`,
                 src: `/memory/card${i}.svg`,
                 chosen: false,
-                className: "cardimg object contain"
+                className: "cardimg object contain",
+                foundMatch: false
             })
         }
         shuffleArray(generatedCards);
@@ -43,16 +63,31 @@ export default function MemoryLogic() {
             array[j] = temp;
         }
     }
+
     function handleCardClick(cardIndex: number) {
-        console.log("hey")
-        setCards(cards.map((card, index) => {
-            console.log(card.chosen)
-            if (index === cardIndex) {
-                return {...card, chosen: !card.chosen};
-            }
-            return card;
-        }));
+        const card = cards[cardIndex];
+
+        if (card.foundMatch || (openCards.length === 2 && openCards.includes(cardIndex))) {
+            return;
+        }
+
+        if (openCards.length === 2) {
+            setCards(prevCards =>
+                prevCards.map((prevCard, index) =>
+                    openCards.includes(index) ? {...prevCard, chosen: false} : prevCard
+                )
+            );
+            setOpenCards([]);
+        }
+
+        setCards(prevCards =>
+            prevCards.map((prevCard, index) =>
+                index === cardIndex ? {...prevCard, chosen: true} : prevCard
+            )
+        );
+        setOpenCards(prevOpenCards => [...prevOpenCards, cardIndex]);
     }
+
 
     return (
         <div className="h-screen w-screen flex items-center justify-center">
@@ -62,7 +97,8 @@ export default function MemoryLogic() {
                          className={`flex h-[150px] w-[150px] bg-white`}
                          onClick={() => handleCardClick(index)}>
                         {card.chosen &&
-                        <img key={index} src={card.src} className={card.className}/>}
+                            <img key={index} src={card.src}
+                                 className={`${card.className} ${card.foundMatch ? " border border-2 border-amber-500" : ""}`}/>}
                     </div>
                 ))}
             </div>
