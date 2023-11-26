21d622aa5f53380fca3293382443c039e5bfe174
anitastipic <mailforanitastipic@gmail.com>
2023-11-26 21:10:55 +0100

    added visibility toggle

--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 type Card = {
     index: string;
@@ -10,6 +10,10 @@ type Card = {
 
 export default function MemoryLogic() {
     const [cards, setCards] = useState<Card[]>([]);
+
+    useEffect(() => {
+        setCards(generateCards(10));
+    }, []);
 
     function generateCards(totalCardsPerType: number) {
         const generatedCards: Card[] = [];
@@ -32,25 +36,34 @@ export default function MemoryLogic() {
     }
 
     function shuffleArray(array: Card[]) {
-        for(let i = array.length - 1; i > 0; i--) {
+        for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * i + 1);
             const temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
     }
-
-    useEffect(() => {
-        setCards(generateCards(10));
-    }, []);
-
+    function handleCardClick(cardIndex: number) {
+        console.log("hey")
+        setCards(cards.map((card, index) => {
+            console.log(card.chosen)
+            if (index === cardIndex) {
+                return {...card, chosen: !card.chosen};
+            }
+            return card;
+        }));
+    }
 
     return (
         <div className="h-screen w-screen flex items-center justify-center">
             <div className="grid grid-cols-5 gap-3">
                 {cards.map((card, index) => (
-                    <div className="flex h-[150px] w-[150px] bg-white"><img key={index} src={card.src}
-                                                                            className={card.className}/></div>
+                    <div key={index}
+                         className={`flex h-[150px] w-[150px] bg-white`}
+                         onClick={() => handleCardClick(index)}>
+                        {card.chosen &&
+                        <img key={index} src={card.src} className={card.className}/>}
+                    </div>
                 ))}
             </div>
         </div>
