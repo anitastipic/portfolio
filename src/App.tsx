8cff2eac32b346561d29fadab57398ad0c6dd2a8
anitastipic <mailforanitastipic@gmail.com>
2023-12-14 22:35:55 +0100

    added home component in app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./components/Home.tsx";
+
+function App() {
+
+    return (
+      <div className="bg-black">
+          <Home></Home>
+      </div>
+    )
+}
+export default App;
