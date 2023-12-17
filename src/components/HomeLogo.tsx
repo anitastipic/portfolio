618190ef30a94fbdc972df08a530de8f22200b06
anitastipic <mailforanitastipic@gmail.com>
2023-12-17 22:44:56 +0100

    changed logo to absolute path

--- src/components/HomeLogo.tsx
+++ src/components/HomeLogo.tsx
@@ -13,7 +13,7 @@ export default function HomeLogo2() {
     return (
         <div className="HomeLogoContainer h-[100vh] w-auto flex flex-col justify-center items-center">
             <video ref={videoRef} className="h-[55vh] w-auto" controls={false} loop={false} autoPlay={true} muted={true}
-                   src={"/ASlogo.mp4"}></video>
+                   src={"public/ASlogo.mp4"}></video>
             <div className="h-[20vh]">
                 <svg className="w-5 h-5 text-amber-50 dark:text-white opacity-50 animate-bounce" aria-hidden="true"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 12">
